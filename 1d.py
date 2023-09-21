71fc0017ca48a5f1e1e35597698170dd7fd6c515
lishaowei <lishaowei@sensetime.com>
2023-09-21 17:22:05 +0800

    transformer size and iou and map and cnn

--- 1d.py
+++ 1d.py
@@ -99,3 +99,28 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 x = torch.randn(1, 1, input_size) 
 y = torch.randn(1, output_size)
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SimpleCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(SimpleCNN, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(32 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+    
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 32 * 8 * 8)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+model = SimpleCNN(num_classes=10)
+
+print(model)
